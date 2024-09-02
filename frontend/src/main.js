94359928a75ab2f79d94eb6357961370f7984707
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2024-09-03 02:43:17 +0600

    frontend: router added

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from '@/router'
 import '@/assets/tailwind.css'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.use(router)
+app.mount('#app')
---
+++ frontend/src/router/index.js
@@ -0,0 +1,17 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+import HomePage from '@/pages/HomePage.vue'
+
+const routes = [
+  {
+    path: '/',
+    component: HomePage
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+
+export default router
