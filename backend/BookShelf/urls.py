86993ebe19fe9ab21cd4136c4430524e3c7eefdb
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2024-09-06 23:32:56 +0600

    backend: swagger configuration

--- backend/BookShelf/urls.py
+++ backend/BookShelf/urls.py
@@ -16,7 +16,30 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Book Shelf",
+        default_version='v1',
+        description="API documentation for Book Shelf",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@example.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # Swagger documentation URLs
+    path('swagger/', schema_view.with_ui('swagger',
+         cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc',
+         cache_timeout=0), name='schema-redoc'),
+
 ]
