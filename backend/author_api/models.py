687219e6f4c990027f03e0bda2ceea795e6d8df0
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2024-09-07 01:43:30 +0600

    backend: author model

--- backend/author_api/models.py
+++ backend/author_api/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    biography = models.TextField(blank=True, null=True)  # Optional bio field
+    birth_date = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
+    @property
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
