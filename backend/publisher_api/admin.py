de0f6bcc7ff826f7193759cac46cb00115e02628
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2024-09-07 02:15:06 +0600

    backend: publisher admin

--- backend/publisher_api/admin.py
+++ backend/publisher_api/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from publisher_api.models import Publisher
+
+
+@admin.register(Publisher)
+class PublisherAdmin(admin.ModelAdmin):
+    list_display = [
+        'name', 'address', 'website',
+        'email', 'phone_number',
+        'established_year',
+    ]
+    list_display_links = [
+        'name',
+    ]
+    list_filter = []
+    search_fields = [
+        'name', 'address', 'website',
+        'email', 'phone_number',
+        'established_year',
+    ]
+    readonly_fields = ['id',]
