011342c069261943df095f99e8a727ec80343fbc
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2024-09-07 03:07:03 +0600

    backend: book admin updated

--- backend/book_api/admin.py
+++ backend/book_api/admin.py
@@ -6,6 +6,19 @@ from book_api.models import Book
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = [
-        'title', 'edition', 'isbn',
+        'id', 'title', 'edition', 'isbn',
+        'published_date', 'language',
         'is_deleted',
     ]
+    list_display_links = [
+        'title',
+    ]
+    list_filter = [
+        'authors', 'publisher',
+    ]
+    search_fields = [
+        'title', 'edition', 'isbn',
+        'published_date', 'language',
+        'description',
+    ]
+    readonly_fields = ['id',]
