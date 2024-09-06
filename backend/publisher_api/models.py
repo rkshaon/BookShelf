d7e2cf3b374e5e41717583101612dfee74ee12b5
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2024-09-07 02:04:15 +0600

    backend: publisher model

--- backend/publisher_api/models.py
+++ backend/publisher_api/models.py
@@ -1,17 +1,17 @@
 from django.db import models
 
 
-# class Publisher(models.Model):
-#     name = models.CharField(max_length=255)
-#     address = models.CharField(max_length=255, blank=True, null=True)
-#     website = models.URLField(blank=True, null=True)
-#     email = models.EmailField(blank=True, null=True)
-#     phone_number = models.CharField(max_length=20, blank=True, null=True)
-#     established_year = models.PositiveIntegerField(blank=True, null=True)
-#     # Contact person at the publisher
-#     contact_person = models.CharField(max_length=255, blank=True, null=True)
-#     country = models.CharField(max_length=100, blank=True, null=True)
-#     city = models.CharField(max_length=100, blank=True, null=True)
+class Publisher(models.Model):
+    name = models.CharField(max_length=255)
+    address = models.CharField(max_length=255, blank=True, null=True)
+    website = models.URLField(blank=True, null=True)
+    email = models.EmailField(blank=True, null=True)
+    phone_number = models.CharField(max_length=20, blank=True, null=True)
+    established_year = models.PositiveIntegerField(blank=True, null=True)
+    # Contact person at the publisher
+    contact_person = models.CharField(max_length=255, blank=True, null=True)
+    country = models.CharField(max_length=100, blank=True, null=True)
+    city = models.CharField(max_length=100, blank=True, null=True)
 
-#     def __str__(self):
-#         return self.name
+    def __str__(self):
+        return self.name
