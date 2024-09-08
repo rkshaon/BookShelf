7a773d006b96f9684788e11257ccfa5eeef73568
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2024-09-08 14:05:11 +0600

    feat: backend - book delete
    signals folder created to hold every signals
    
    on delete book, a signal delete the book file, cover image physically from the file directory

--- backend/book_api/apps.py
+++ backend/book_api/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class BookApiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'book_api'
+
+    def ready(self) -> None:
+        import book_api.signals # noqa
+        return super().ready()
