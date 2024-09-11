1d0f3e3ac55bf6b4a6c47a860aa5d9cb644fc69c
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2024-09-12 01:54:15 +0600

    frontend, backend
    backend - replace storage file function updated
    
    frontend - book card component updated for handle iamges

--- frontend/src/components/BookCardComponent.vue
+++ frontend/src/components/BookCardComponent.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="bg-white shadow-lg rounded-lg overflow-hidden">
-    <img :src="book.coverImage" alt="Book Cover" class="w-full h-48 object-cover">
+    <!-- <img :src="book.cover_image" alt="Book Cover" class="w-full h-48 object-cover"> -->
+    <img :src="getCoverImageUrl(book.cover_image)" alt="Book Cover" class="w-full h-48 object-cover">
     <div class="p-4">
       <h3 class="text-lg font-bold text-gray-800">{{ book.title }}</h3>
       <!-- <p class="text-gray-600">{{ book.authors.join(', ') }}</p> -->
@@ -16,6 +17,12 @@ export default {
     book: {
       type: Object,
       required: true
+    }
+  },
+  methods: {
+    getCoverImageUrl (coverImage) {
+      const baseURL = 'http://localhost:8001/'
+      return coverImage ? `${baseURL}${coverImage}` : 'https://fastly.picsum.photos/id/1/200/300.jpg?hmac=jH5bDkLr6Tgy3oAg5khKCHeunZMHq0ehBZr6vGifPLY' // Fallback to default image if cover_image is null
     }
   }
 }
