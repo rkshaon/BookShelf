bbd6f923616962639da19144b5e385a1d1766db9
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2024-09-11 13:07:50 +0600

    updt: backend - author serializer
    remove not required fields and added full_name field

--- backend/author_api/serializers/v1.py
+++ backend/author_api/serializers/v1.py
@@ -6,4 +6,7 @@ from author_api.models import Author
 class AuthorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Author
-        fields = '__all__'
+        fields = [
+            'id', 'full_name', 'biography', 'birth_date',
+            'died_date',
+        ]
