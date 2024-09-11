ab748d8792c06180ff33c5d909040ddbdf4c345a
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2024-09-11 15:27:09 +0600

    updt: backend - setting

--- backend/BookShelf/settings.py
+++ backend/BookShelf/settings.py
@@ -147,7 +147,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
