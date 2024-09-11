da0df9a0cb7a308e1f1211023f7d0be9bc258296
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2024-09-12 00:02:11 +0600

    feat: frontend - 404 page
    not found page added with routing

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,13 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 import HomePage from '@/pages/HomePage.vue'
+import NotFoundPage from '@/pages/NotFoundPage.vue';
 
 const routes = [
   {
-    path: '/',
-    component: HomePage
-  }
-]
+    path: "/",
+    component: HomePage,
+  },
+  { path: "/:pathMatch(.*)*", name: "NotFound", component: NotFoundPage },
+];
 
 const router = createRouter({
   history: createWebHistory(),
