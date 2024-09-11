c1ffebc6e4236fc4f65edb4a3312565802506ebc
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2024-09-11 13:03:44 +0600

    updt: backend - book serializer
    authors full object added

--- backend/book_api/serializers/v1.py
+++ backend/book_api/serializers/v1.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
 
+from author_api.serializers.v1 import AuthorSerializer
+
 from book_api.models import Genre
 from book_api.models import Book
 
@@ -14,3 +16,10 @@ class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
         fields = '__all__'
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        representation['authors'] = AuthorSerializer(
+            instance.authors, many=True).data
+
+        return representation
