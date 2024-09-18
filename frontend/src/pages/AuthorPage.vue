dec34c02d2aee1c972b76d31101a2840f5079ae8
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2024-09-19 02:27:52 +0600

    frontend: autho page
    api call, data display done

--- frontend/src/pages/AuthorPage.vue
+++ frontend/src/pages/AuthorPage.vue
@@ -1,3 +1,85 @@
 <template>
-    <div>ok</div>
+    <div class="container mx-auto py-8 px-4 sm:px-6 lg:px-8">
+        <div v-if="isLoading" class="flex justify-center">
+            <LoaderComponent />
+        </div>
+        <div v-else-if="author" class="bg-white shadow-lg rounded-lg p-6 grid grid-cols-1 md:grid-cols-2 gap-8">
+            <div class="flex justify-center">
+                <!-- <img :src="getAuthorImage(author.image)" alt="Author Image"
+                    class="w-40 h-40 object-cover rounded-full shadow-lg" /> -->
+            </div>
+            <div>
+                <h1 class="text-3xl font-bold text-gray-800 mb-4">{{ author.full_name }}</h1>
+                <p class="text-gray-600 mb-4"><strong>Biography:</strong> {{ author.biography || 'N/A' }}</p>
+                <p class="text-gray-600 mb-4">
+                    <strong>Status:</strong>
+                    <span v-if="author.is_alive">Alive</span>
+                    <span v-else>Deceased
+                        ({{ author.died_date }})
+                    </span>
+                </p>
+                <p class="text-gray-600 mb-4">
+                    <strong>Born:</strong> {{ author.birth_date || 'Unknown' }}
+                </p>
+                <!-- <div v-if="author.books.length > 0" class="mt-6">
+                    <h3 class="text-xl font-semibold text-gray-700 mb-4">Books by {{ author.full_name }}</h3>
+                    <ul class="list-disc list-inside text-gray-600">
+                        <li v-for="book in author.books" :key="book.title" class="mb-2">
+                            <router-link :to="{ name: 'Book', params: { book_code: book.book_code } }"
+                                class="text-blue-500 hover:underline">
+                                {{ book.title }} - {{ book.published_date || 'Unknown' }}
+                            </router-link>
+                        </li>
+                    </ul>
+                </div> -->
+            </div>
+        </div>
+        <div v-else-if="error" class="text-red-500">
+            <NotFoundComponent contentType="Author" :errorMessage="error" />
+        </div>
+    </div>
 </template>
+
+<script>
+// import { mapActions } from 'vuex'
+import { mapGetters, mapActions } from 'vuex'
+import LoaderComponent from '@/components/LoaderComponent.vue'
+import NotFoundComponent from '@/components/NotFoundComponent.vue'
+
+export default {
+  name: 'AuthorPage',
+  components: {
+    LoaderComponent,
+    NotFoundComponent
+  },
+  computed: {
+    ...mapGetters({
+      author: 'getAuthorDetails',
+      isLoading: 'isAuthorLoading',
+      error: 'authorDetailsError'
+    })
+  },
+  watch: {
+    author (newAuthor) {
+      if (newAuthor && newAuthor.full_name) {
+        document.title = `Book Shelf | ${newAuthor.full_name}`
+      }
+    }
+  },
+  mounted () {
+    const authorId = this.$route.params.id
+    this.fetchAuthorDetails(authorId)
+    document.title = 'Book Shelf | Loading ...'
+  },
+  methods: {
+    ...mapActions(['fetchAuthorDetails'])
+    // getAuthorImage (image) {
+    //   return image ? `${process.env.VUE_APP_BACKEND_URL}/media/${image}` : '/default-author.png'
+    // }
+  }
+}
+</script>
+
+<style scoped>
+/* Custom styles for AuthorDetailsPage.vue */
+</style>
--- frontend/src/store/modules/authors/authorDetails.js
+++ frontend/src/store/modules/authors/authorDetails.js
@@ -1,50 +1,48 @@
 // src/store/modules/bookDetails.js
 
-import authorAPIService from "@/services/v1/authorAPIService";
-
-const state = {
-  authorDetails: null,
-  isLoading: false,
-  error: null,
-};
-
-const getters = {
-  getAuthorDetails: (state) => state.authorDetails,
-  isAuthorLoading: (state) => state.isLoading,
-  authorDetailsError: (state) => state.error,
-};
-
-const actions = {
-  async fetchAuthorDetails({ commit }, authorId) {
-    commit("SET_LOADING", true);
-    commit("SET_ERROR", null);
-    try {
-      const response = await authorAPIService.fetchV1AuthorDetails(authorId);
-      commit("SET_AUTHOR_DETAILS", response.data);
-    } catch (error) {
-      commit("SET_ERROR", "Error fetching author details");
-    } finally {
-      commit("SET_LOADING", false);
-    }
-  },
-};
-
-const mutations = {
-  SET_AUTHOR_DETAILS(state, author) {
-    state.authorDetails = author;
-  },
-  SET_LOADING(state, isLoading) {
-    state.isLoading = isLoading;
-  },
-  SET_ERROR(state, error) {
-    state.error = error;
-  },
-};
+import authorAPIService from '@/services/v1/authorAPIService'
 
 export default {
-  namespaced: true,
-  state,
-  getters,
-  actions,
-  mutations,
-};
+  state: {
+    authorDetails: null,
+    isLoading: false,
+    error: null
+  },
+  getters: {
+    getAuthorDetails: (state) => state.authorDetails,
+    isAuthorLoading: (state) => state.isLoading,
+    authorDetailsError: (state) => state.error
+  },
+  mutations: {
+    SET_AUTHOR_DETAILS (state, author) {
+      state.authorDetails = author
+    },
+    SET_LOADING (state, isLoading) {
+      console.log('just testing')
+
+      state.isLoading = isLoading
+      console.log('loading value', state.isLoading)
+    },
+    SET_ERROR (state, error) {
+      state.error = error
+    }
+  },
+  actions: {
+    async fetchAuthorDetails ({ commit }, authorId) {
+      commit('SET_LOADING', true)
+      commit('SET_ERROR', null)
+      try {
+        const response = await authorAPIService.fetchV1AuthorDetails(authorId)
+        console.log('response:', response)
+        commit('SET_AUTHOR_DETAILS', response)
+      } catch (error) {
+        commit('SET_ERROR', 'Error fetching author details')
+      } finally {
+        // console.log('loading...', this.state.isLoading)
+        commit('SET_LOADING', false)
+        //   console.log("loading...", this.state.isLoading)
+      }
+    }
+  }
+
+}
