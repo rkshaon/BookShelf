4d9b8382975b54f1e268837b112e449c6e850bc2
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2024-09-22 02:10:50 +0600

    feat: frontend - topic page
    complete

--- frontend/src/pages/TopicPage.vue
+++ frontend/src/pages/TopicPage.vue
@@ -1,10 +1,5 @@
 <template>
     <div class="container mx-auto py-8 px-4 sm:px-6 lg:px-8">
-        <!-- <div class="bg-white shadow rounded-lg p-6 mb-8">
-            <h1 v-if="topic" class="text-4xl font-bold text-center text-gray-800 mb-4">
-                {{ topic.name }}
-            </h1>
-        </div> -->
         <div v-if="isTopicLoading" class="flex justify-center">
                 <LoaderComponent />
             </div>
@@ -17,7 +12,7 @@
                 <h2 class="text-2xl font-semibold text-gray-800 mb-6">
                     Books on #{{ topic.slug }}
                 </h2>
-                <!-- <div v-if="isBookLoading" class="flex justify-center">
+                <div v-if="isBookLoading" class="flex justify-center">
                     <LoaderComponent />
                 </div>
                 <div v-else>
@@ -26,7 +21,7 @@
                     <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
                         <BookCardComponent v-for="(book, index) in books" :key="index" :book="book" />
                     </div>
-                </div> -->
+                </div>
             </div>
             <div v-else-if="error" class="text-red-500">
                 <NotFoundComponent contentType="Genre" :errorMessage="error" />
@@ -37,17 +32,17 @@
 <script>
 import { mapGetters, mapActions } from 'vuex'
 import LoaderComponent from '@/components/LoaderComponent.vue'
-// import PaginationComponent from '@/components/PaginationComponent.vue'
-// import BookCardComponent from '@/components/BookCardComponent.vue'
+import PaginationComponent from '@/components/PaginationComponent.vue'
+import BookCardComponent from '@/components/BookCardComponent.vue'
 import NotFoundComponent from '@/components/NotFoundComponent.vue'
 
 export default {
   name: 'TopicPage',
   components: {
     LoaderComponent,
-    NotFoundComponent
-    // PaginationComponent,
-    // BookCardComponent
+    NotFoundComponent,
+    PaginationComponent,
+    BookCardComponent
   },
   data () {
     return {
@@ -63,62 +58,63 @@ export default {
       nextPageUrl: 'nextPageUrl',
       previousPageUrl: 'previousPageUrl',
       currentPageSize: 'currentPageSize'
-    })
-    // books() {
-    //     return this.allBooks
-    // },
-    // pageSize() {
-    //     return this.currentPageSize
-    // }
+    }),
+    books () {
+      return this.allBooks
+    },
+    pageSize () {
+      return this.currentPageSize
+    }
   },
   watch: {
     topic (newTopic) {
       if (newTopic && newTopic.name) {
         document.title = `Book Shelf | ${newTopic.name}`
       }
+    },
+    '$route.query.page': {
+      immediate: true,
+      handler (newPage) {
+        let page = parseInt(newPage, 10)
+
+        if (!page || isNaN(page)) {
+          this.$router.replace({ query: { page: 1 } })
+          page = 1
+          return
+        }
+
+        this.fetchBooks({ page, pageSize: this.pageSize, topic: this.$route.params.id })
+      }
     }
-    // '$route.query.page': {
-    //     immediate: true,
-    //     handler(newPage) {
-    //         let page = parseInt(newPage, 10)
-
-    //         if (!page || isNaN(page)) {
-    //             this.$router.replace({ query: { page: 1 } })
-    //             page = 1
-    //             return
-    //         }
-
-    //         this.fetchBooks({ page, pageSize: this.pageSize, genre: this.$route.params.id })
-    //     }
-    // }
   },
   mounted () {
     const topicId = this.$route.params.id
-    // const currentPage = this.$route.query.page
+    const currentPage = this.$route.query.page
 
-    // if (!currentPage || isNaN(currentPage)) {
-    //     this.$router.replace({ query: { page: 1 } })
-    // }
+    if (!currentPage || isNaN(currentPage)) {
+      this.$router.replace({ query: { page: 1 } })
+    }
 
     this.fetchTopicDetails(topicId)
     document.title = 'Book Shelf | Loading ...'
   },
   methods: {
-    ...mapActions(['fetchTopicDetails', 'fetchBooks'])
-    // async fetchBooks(payload) {
-    //     this.isBookLoading = true
-    //     this.$store.commit('SET_BOOKS', [])
-    //     try {
-    //         await this.$store.dispatch('fetchBooks', payload)
-    //     } catch (error) {
-    //         console.error('Error fetching books:', error)
-    //     } finally {
-    //         this.isBookLoading = false
-    //     }
-    // },
-    // changePage(page) {
-    //     this.$router.push({ query: { page } })
-    // }
+    ...mapActions(['fetchTopicDetails', 'fetchBooks']),
+    async fetchBooks (payload) {
+      console.log(payload)
+      this.isBookLoading = true
+      this.$store.commit('SET_BOOKS', [])
+      try {
+        await this.$store.dispatch('fetchBooks', payload)
+      } catch (error) {
+        console.error('Error fetching books:', error)
+      } finally {
+        this.isBookLoading = false
+      }
+    },
+    changePage (page) {
+      this.$router.push({ query: { page } })
+    }
   }
 }
 </script>
