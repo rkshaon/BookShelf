622c3d5ff3fee3c778c23ea314be31e16d0307e0
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2024-09-24 10:32:02 +0600

    wip: elastic search - backend
    publish model prepared

---
+++ backend/publisher_api/documents.py
@@ -0,0 +1,19 @@
+from django_elasticsearch_dsl import Document, Index
+from django_elasticsearch_dsl.registries import registry
+
+from publisher_api.models import Publisher
+
+
+publisher_index = Index('publishers')
+
+
+@registry.register
+class PublisherDocument(Document):
+    class Index:
+        name = 'publishers'
+
+    class Django:
+        model = Publisher
+        fields = [
+            'name',
+        ]
