e5aaccf57087278899426ebd2785c03a80b8b664
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2024-10-22 00:49:38 +0600

    frontend: search item component
    design done
    
    on click navigate to the book

---
+++ frontend/src/components/SearchItemComponent.vue
@@ -0,0 +1,34 @@
+<template>
+    <li @click="handleClick" class="px-4 py-2 hover:bg-gray-100 cursor-pointer">
+        <h3 class="font-semibold">{{ result.title }}</h3>
+        <p class="text-sm text-gray-600">
+            <span v-for="(author, index) in result.authors" :key="index">
+                {{ author.full_name }}<span v-if="index < result.authors.length - 1">, </span>
+            </span>
+        </p>
+    </li>
+</template>
+
+<script>
+export default {
+  name: 'SearchResultItem',
+  props: {
+    result: {
+      type: Object,
+      required: true
+    }
+  },
+  methods: {
+    handleClick () {
+      // Emit an event or perform any action when the item is clicked
+      this.$emit('select-result', this.result)
+      console.log('clickked')
+      this.$router.push(`/book/${this.result.book_code}`)
+    }
+  }
+}
+</script>
+
+<style scoped>
+/* Custom styles for the search result item */
+</style>
