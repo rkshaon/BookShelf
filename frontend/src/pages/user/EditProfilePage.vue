2ffe0b55063e7c49a7e78a5d9dd0dad576ae6fb0
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2024-11-07 14:58:22 +0600

    wip: profile edit data prepared

--- frontend/src/pages/user/EditProfilePage.vue
+++ frontend/src/pages/user/EditProfilePage.vue
@@ -1,48 +1,43 @@
 <template>
-    <div class="min-h-screen bg-gray-100 flex flex-col items-center">
-        <div class="bg-white shadow-md rounded-lg p-6 mt-10 w-full max-w-md">
-            <div v-if="isLoading" class="flex justify-center">
-                <LoaderComponent />
+  <div class="min-h-screen bg-gray-100 flex flex-col items-center">
+    <div class="bg-white shadow-md rounded-lg p-6 mt-10 w-full max-w-md">
+      <div v-if="isLoading" class="flex justify-center">
+        <LoaderComponent />
+      </div>
+      <div v-else>
+        <form @submit.prevent="editProfile">
+          <div class="flex flex-col items-center">
+            <img class="w-24 h-24 rounded-full object-cover" :src="getProfileImage(user.profile_image, API_BASE_URL)"
+              alt="User Avatar" />
+          </div>
+          <div class="mt-6">
+            <h3 class="text-lg font-medium">Profile Information</h3>
+            <div class="mt-4">
+              <label class="block text-gray-700">First Name</label>
+              <input type="text" class="mt-1 block w-full border-gray-300 rounded-md shadow-sm"
+                v-model="user.first_name" />
             </div>
-            <div v-else>
-                <div class="flex flex-col items-center">
-                    <img class="w-24 h-24 rounded-full object-cover"
-                        :src="getProfileImage(user.profile_image, API_BASE_URL)" alt="User Avatar" />
-                    <h2 class="mt-4 text-xl font-semibold">{{ user.full_name || 'Full Name' }}</h2>
-                    <p class="text-gray-600">{{ user.email || 'yourmail@example.com' }}</p>
-                    <p class="text-gray-600">{{ user.role_display || 'User Role' }}</p>
-                </div>
-                <div class="mt-6">
-                    <h3 class="text-lg font-medium">Profile Information</h3>
-                    <div class="mt-4">
-                        <label class="block text-gray-700">First Name</label>
-                        <input type="text" class="mt-1 block w-full border-gray-300 rounded-md shadow-sm"
-                            :value="user.first_name" />
-                    </div>
-                    <div class="mt-4">
-                        <label class="block text-gray-700">Middle Name</label>
-                        <input type="text" class="mt-1 block w-full border-gray-300 rounded-md shadow-sm"
-                            :value="user.middle_name" />
-                    </div>
-                    <div class="mt-4">
-                        <label class="block text-gray-700">Last Name</label>
-                        <input type="text" class="mt-1 block w-full border-gray-300 rounded-md shadow-sm"
-                            :value="user.last_name" />
-                    </div>
-                    <div class="mt-4">
-                        <label class="block text-gray-700">Email</label>
-                        <input type="email" class="mt-1 block w-full border-gray-300 rounded-md shadow-sm"
-                            :value="user.email" />
-                    </div>
-                    <div class="mt-6 flex justify-end">
-                        <button class="bg-blue-500 text-white px-4 py-2 rounded-md shadow-sm hover:bg-blue-600">
-                            Save Changes
-                        </button>
-                    </div>
-                </div>
+            <div class="mt-4">
+              <label class="block text-gray-700">Middle Name</label>
+              <input type="text" class="mt-1 block w-full border-gray-300 rounded-md shadow-sm"
+                v-model="user.middle_name" />
             </div>
-        </div>
+            <div class="mt-4">
+              <label class="block text-gray-700">Last Name</label>
+              <input type="text" class="mt-1 block w-full border-gray-300 rounded-md shadow-sm"
+                v-model="user.last_name" />
+            </div>
+            <div class="mt-6 flex justify-end">
+              <button class="bg-blue-500 text-white px-4 py-2 rounded-md shadow-sm hover:bg-blue-600">
+                Save Changes
+              </button>
+            </div>
+          </div>
+        </form>
+
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
@@ -77,7 +72,11 @@ export default {
   },
   methods: {
     ...mapActions(['fetchUserProfile']),
-    getProfileImage
+    getProfileImage,
+    async editProfile () {
+      console.log('Edit Profile')
+      console.log(this.user)
+    }
   },
   watch: {
     user (newProfile) {
