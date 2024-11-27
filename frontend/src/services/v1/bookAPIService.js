53bde6e1b445137ff1912cca9a35326cc1ecb281
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2024-11-28 02:23:08 +0600

    updt: book api service axios to api convert

--- frontend/src/services/v1/bookAPIService.js
+++ frontend/src/services/v1/bookAPIService.js
@@ -1,6 +1,6 @@
 // src/services/v1/bookAPIService.js
 
-import axios from 'axios'
+import api from '@/services/axiosInstance'
 
 const API_BASE_URL = process.env.VUE_APP_BACKEND_URL
 const content = 'book'
@@ -26,7 +26,7 @@ export const fetchV1Books = async (
   console.log('Books API:', URL, page, pageSize)
 
   try {
-    const response = await axios.get(URL)
+    const response = await api.get(URL)
     return response.data
   } catch (error) {
     console.error('Error fetching books:', error)
@@ -39,7 +39,7 @@ export const fetchV1BookDetails = async (bookCode = null) => {
   console.log('Book Details API:', URL)
 
   try {
-    const response = await axios.get(URL)
+    const response = await api.get(URL)
     return response.data
   } catch (error) {
     console.error('Error fetching book:', error)
@@ -59,11 +59,27 @@ export const searchV1Books = async (query = '', page = 1, pageSize = 8) => {
   console.log('Search Books API:', URL)
 
   try {
-    const response = await axios.get(URL)
+    const response = await api.get(URL)
     // console.log(response.data)
     return response.data
   } catch (error) {
     console.error('Error searching books:', error)
+    throw error
+  }
+}
+
+export const createV1Book = async (data) => {
+  const URL = `${API_BASE_URL}/${content}/${version}/`
+  try {
+    const response = await api.post(URL, data)
+    return response.data
+  } catch (error) {
+    console.error('Error creating book at service:', error)
+
+    if (error.response && error.response.status === 403) {
+      return { error: true, message: 'Permission denied to create an author' }
+    }
+
     throw error
   }
 }
