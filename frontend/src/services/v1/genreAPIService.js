63e36b0af65e2d5130c2190fe9902bdd11945445
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2024-11-29 19:42:04 +0600

    updt: genre api calling converted

--- frontend/src/services/v1/genreAPIService.js
+++ frontend/src/services/v1/genreAPIService.js
@@ -6,17 +6,29 @@ const API_BASE_URL = process.env.VUE_APP_BACKEND_URL
 const content = 'genre'
 const version = 'v1'
 
-export default {
-  async fetchV1GenreDetails (id = null) {
-    const URL = `${API_BASE_URL}/book/${version}/${content}/${id}/`
-    console.log('Genre Details API:', URL)
+export const fetchV1GenreDetails = async (id = null) => {
+  const URL = `${API_BASE_URL}/book/${version}/${content}/${id}/`
+  console.log('Genre Details API:', URL)
 
-    try {
-      const response = await axios.get(URL)
-      return response.data
-    } catch (error) {
-      console.error('Error fetching genre:', error)
-      throw error
-    }
+  try {
+    const response = await axios.get(URL)
+    return response.data
+  } catch (error) {
+    console.error('Error fetching genre:', error)
+    throw error
   }
 }
+// export default {
+//   async fetchV1GenreDetails (id = null) {
+//     const URL = `${API_BASE_URL}/book/${version}/${content}/${id}/`
+//     console.log('Genre Details API:', URL)
+
+//     try {
+//       const response = await axios.get(URL)
+//       return response.data
+//     } catch (error) {
+//       console.error('Error fetching genre:', error)
+//       throw error
+//     }
+//   }
+// }
