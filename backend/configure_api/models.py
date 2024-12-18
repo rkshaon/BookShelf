354aecdb176fc33bcda2a1ee0953f97d574cf503
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2024-12-18 12:00:49 +0600

    wip: #213 - timezone model added

--- backend/configure_api/models.py
+++ backend/configure_api/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+import pytz
+
+
+class Timezone(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    @staticmethod
+    def populate_timezones():
+        """Populates the Timezone table with all pytz timezones."""
+        for tz in pytz.all_timezones:
+            Timezone.objects.get_or_create(name=tz)
