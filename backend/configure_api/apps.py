edb53820f6333f8c10489fdb1276ab68ab0b4738
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2024-12-18 12:12:54 +0600

    wip: #213 - timezone model data populated

--- backend/configure_api/apps.py
+++ backend/configure_api/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class ConfigureApiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'configure_api'
+    verbose_name = 'Configure'
