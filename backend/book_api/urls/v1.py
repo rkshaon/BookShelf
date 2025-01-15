3c9539b552a15ba1a5c5d59e2672c9d0d20e0f5c
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2025-01-16 01:57:55 +0600

    wip: bs-194 - migrating book APIs

--- backend/book_api/urls/v1.py
+++ backend/book_api/urls/v1.py
@@ -1,4 +1,4 @@
-from django.urls import path
+# from django.urls import path
 from rest_framework.routers import DefaultRouter
 
 from book_api.views import v1
@@ -6,13 +6,14 @@ from book_api.views import v1
 
 router = DefaultRouter()
 
+router.register('', v1.BookViewSet, basename='book')
 router.register('genre', v1.GenreViewSet, basename='genre')
 router.register('topic', v1.TopicViewSet, basename='topic')
 
 urlpatterns = [
-    path('', v1.BookView.as_view()),
-    path('<int:book_code>/', v1.BookView.as_view()),
-    path('search', v1.BookSearchView.as_view()),
+    # path('', v1.BookView.as_view()),
+    # path('<int:book_code>/', v1.BookView.as_view()),
+    # path('search', v1.BookSearchView.as_view()),
 ]
 
 urlpatterns += router.urls
--- backend/book_api/views/v1/book.py
+++ backend/book_api/views/v1/book.py
@@ -1,4 +1,5 @@
 from rest_framework.views import APIView
+from rest_framework.viewsets import ModelViewSet
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
 from rest_framework.exceptions import NotFound
@@ -8,6 +9,7 @@ from django.utils.decorators import method_decorator
 
 from BookShelf.utilities.pagination import Pagination
 from BookShelf.utilities.permissions import IsAdminOrModerator
+from BookShelf.utilities.filters import SearchFilter
 from activity_api.utilities.event import event_logger
 
 from book_api.models import Book
@@ -15,69 +17,81 @@ from book_api.models import Book
 from book_api.serializers.v1 import BookSerializer
 
 
-@method_decorator(cache_page(60*1), name='get')
-class BookView(APIView):
-    permission_classes = [AllowAny]
+class BookViewSet(ModelViewSet):
+    serializer_class = BookSerializer
+    queryset = Book.objects.filter(is_deleted=False)
+    permission_classes = [
+        IsAdminOrModerator,
+    ]
+    filter_backends = [SearchFilter]
+    search_fields = ['title']
 
-    def get(self, request, *args, **kwargs):
-        book_code = kwargs.get('book_code', None)
-        genre = request.query_params.get('genre', None)
-        topic = request.query_params.get('topic', None)
 
-        if book_code:
-            try:
-                book = Book.objects.get(book_code=book_code, is_deleted=False)
-            except Book.DoesNotExist:
-                raise NotFound(detail="Book not found.")
 
-            event_logger(
-                event='retrieve',
-                object='book',
-                user=request.user,
-                device=request.device,
-                ip_address=request.ip_address,
-                data={
-                    'model': 'Book',
-                    'id': book.id
-                }
-            )
 
-            return Response(BookSerializer(book).data)
+# @method_decorator(cache_page(60*1), name='get')
+# class BookView(APIView):
+#     permission_classes = [AllowAny]
 
-        books = Book.objects.filter(
-            is_deleted=False
-        )
+#     def get(self, request, *args, **kwargs):
+#         book_code = kwargs.get('book_code', None)
+#         genre = request.query_params.get('genre', None)
+#         topic = request.query_params.get('topic', None)
 
-        if genre:
-            books = books.filter(genres__id=genre)
+#         if book_code:
+#             try:
+#                 book = Book.objects.get(book_code=book_code, is_deleted=False)
+#             except Book.DoesNotExist:
+#                 raise NotFound(detail="Book not found.")
 
-        if topic:
-            books = books.filter(topics__id=topic)
+#             event_logger(
+#                 event='retrieve',
+#                 object='book',
+#                 user=request.user,
+#                 device=request.device,
+#                 ip_address=request.ip_address,
+#                 data={
+#                     'model': 'Book',
+#                     'id': book.id
+#                 }
+#             )
 
-        books = books.order_by('-id')
-        paginator = Pagination()
-        page = paginator.paginate_queryset(books, request)
+#             return Response(BookSerializer(book).data)
 
-        event_logger(
-            event='retrieve',
-            object='book',
-            user=request.user,
-            device=request.device,
-            ip_address=request.ip_address,
-        )
+#         books = Book.objects.filter(
+#             is_deleted=False
+#         )
 
-        return paginator.get_paginated_response(
-            BookSerializer(page, many=True).data
-        )
+#         if genre:
+#             books = books.filter(genres__id=genre)
 
-    def post(self, request, *args, **kwargs):
-        self.permission_classes = [IsAdminOrModerator]
-        self.check_permissions(request)
-        request.data['added_by'] = request.user.id
-        serializer = BookSerializer(
-            data=request.data,
-        )
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
+#         if topic:
+#             books = books.filter(topics__id=topic)
 
-        return Response(serializer.data, status=201)
+#         books = books.order_by('-id')
+#         paginator = Pagination()
+#         page = paginator.paginate_queryset(books, request)
+
+#         event_logger(
+#             event='retrieve',
+#             object='book',
+#             user=request.user,
+#             device=request.device,
+#             ip_address=request.ip_address,
+#         )
+
+#         return paginator.get_paginated_response(
+#             BookSerializer(page, many=True).data
+#         )
+
+#     def post(self, request, *args, **kwargs):
+#         self.permission_classes = [IsAdminOrModerator]
+#         self.check_permissions(request)
+#         request.data['added_by'] = request.user.id
+#         serializer = BookSerializer(
+#             data=request.data,
+#         )
+#         serializer.is_valid(raise_exception=True)
+#         serializer.save()
+
+#         return Response(serializer.data, status=201)
